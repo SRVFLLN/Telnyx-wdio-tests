c304befe857eb948ba722403a97f8a587f5e8687
SRVFLLN <servefallen@gmail.com>
2022-09-28 18:37:08 +0300

    'Trying to fix chrome running in docker'

--- wdio.chrome.js
+++ wdio.chrome.js
@@ -1,3 +1,4 @@
+import platform from "platform";
 import mainPage from "./test/pageobjects/main.page";
 
 exports.config = {
@@ -134,7 +135,9 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    services: ['chromedriver',{
+        path: ()=>{return platform.os.family=='Linux'? '/usr/lib/chromium' : null}
+    }],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
