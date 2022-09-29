9726203956e29bec1bc1ef30fb12fc80dab4cdb3
SRVFLLN <servefallen@gmail.com>
2022-09-29 16:39:24 +0300

    chromedriver path

--- wdio.chrome_docker.js
+++ wdio.chrome_docker.js
@@ -138,7 +138,7 @@ exports.config = {
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
     services: [['chromedriver',{
-        chromedriverCustomPath: path.join('/usr/bin/chromedriver', 'chromedriver')
+        chromedriverCustomPath: '/usr/bin/chromedriver'
     }]],
     
     // Framework you want to run your specs with.
